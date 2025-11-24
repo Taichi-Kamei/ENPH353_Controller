4b4b8d3ab94f7104b88ed061c84db3a53e94252d
Ta1k25 <t.kamei0716@gmail.com>
2025-11-23 16:30:25 -0800

    added comments on  conteroller_GUI.py

--- node/controller_GUI.py
+++ node/controller_GUI.py
@@ -16,7 +16,13 @@ from sensor_msgs.msg import Image
 from std_srvs.srv import Empty
 
 
-
+##
+# Backend for the "./Controller_GUI.ui" using Qt5.
+# Allows control over the Gazebo simulation world, launch scripts for
+# running the score tracker and the robot,and view robot's raw/contour/clue_detection camera view
+#
+# Requires user to launch the Gazebo simulation beforehand.
+#
 class Controller_App(QtWidgets.QMainWindow):
 
     def __init__(self):
@@ -41,7 +47,7 @@ class Controller_App(QtWidgets.QMainWindow):
         self.sub_raw_view = rospy.Subscriber("/B1/rrbot/camera1/image_raw",
                                             Image, self.raw_image_cb, queue_size=1)
         self.sub_contour_view = rospy.Subscriber("/processed_img",
-                                                 Image, self.contour_image_cb, queue_size= 1)
+                                            Image, self.contour_image_cb, queue_size= 1)
         # TODO Add subscriber for clue detection
         #self.sub_clue_view = rospy.Subscriber("/processed_img", Image, self.clue_detection_image_cb, queue_size= 1)
 
@@ -50,28 +56,44 @@ class Controller_App(QtWidgets.QMainWindow):
         self.resume_icon = QtGui.QIcon("icons/icons8-resume-button-30.png")
         self.sim_time_button.setIcon(self.pause_icon)
 
-
+    ##
+    # Callback function for the view_type: Raw
+    # 
+    # @param self The object pointer
+    # @param data The image which is in imgmsg from ROS
     def raw_image_cb(self, data):
-            self.raw_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.raw_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
 
             if self.view_type_button.currentText() == "Raw":
                 self.update_view(self.raw_image)
     
-
+    ##
+    # Callback function for the view_type: Contour
+    # 
+    # @param self The object pointer
+    # @param data The image which is in imgmsg from ROS
     def contour_image_cb(self, data):
-        self.contour_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.contour_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
 
         if self.view_type_button.currentText() == "Contour":
                 self.update_view(self.contour_image)
 
 
+    ##
+    # Callback function for the view_type: Clue
+    # 
+    # @param self The object pointer
+    # @param data The image which is in imgmsg from ROS
     def clue_detection_image_cb(self, data):
-        self.clue_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.clue_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
 
         if self.view_type_button.currentText() == "Clue":
                 self.update_view(self.clue)
 
-
+    ##
+    # Changes the type of view by the type chosen in the combobox on GUI
+    # 
+    # @param self The object pointer
     def change_view_type(self):
         type = self.view_type_button.currentText()
 
@@ -84,7 +106,11 @@ class Controller_App(QtWidgets.QMainWindow):
         elif type == "Clue":
             self.update_view(self.contour_image)
 
-
+    ##
+    # Updates the displayed view on the GUI
+    # 
+    # @param self The object pointer
+    # @param img The image that will be shown on the GUI
     def update_view(self, img):
         if img is None:
             return
@@ -100,7 +126,7 @@ class Controller_App(QtWidgets.QMainWindow):
     # Converts openCV image to QtGui with correct scaling
     # Source: stackoverflow.com/questions/34232632/
     #
-    #  @param self The object pointer
+    # @param self The object pointer
     # @param cv_img Image as a openCV object
     # @return Type QtGui image
     def convert_cv_to_pixmap(self, cv_img):
@@ -111,7 +137,10 @@ class Controller_App(QtWidgets.QMainWindow):
                         bytesPerLine, QtGui.QImage.Format_RGB888)
         return QtGui.QPixmap.fromImage(q_img)
   
-
+    ##
+    # Pauses or unpauses the Gazebo simulation.
+    # 
+    # @param self The object pointer
     def SLOT_sim_time(self):
         
         if self.sim_paused is False:
@@ -126,18 +155,25 @@ class Controller_App(QtWidgets.QMainWindow):
             self.sim_time_button.setIcon(self.pause_icon)
             self.sim_paused = False
 
-
+    ##
+    # Resets the robot position to the initial position
+    # 
+    # @param self The object pointer
     def SLOT_reset_robot_position(self):
         rospy.wait_for_service('/gazebo/set_model_state')
         reset_service = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
-        #TODO Figure out the reset position
+        
         state = ModelState()
         state.model_name = "B1"
-        state.pose.position.x = -8
-        state.pose.position.y = -8
-        state.pose.position.z = 0
-        state.pose.orientation.w = 1
+        state.pose.position.x = 5.5
+        state.pose.position.y = 2.5
+        state.pose.position.z = 0.1
+
+        state.pose.orientation.x = 0
+        state.pose.orientation.y = 0
+        state.pose.orientation.z = 0.707 #Chat did the math for me to make the robot face up
+        state.pose.orientation.w = -0.707
         state.reference_frame = "world"
 
         try:
@@ -146,22 +182,53 @@ class Controller_App(QtWidgets.QMainWindow):
         except:
             rospy.loginfo("Failed to reset model")    
     
-    
-    def SLOT_launch_score_tracker_script(self):
-        script = os.path.expanduser(
-            "~/ros_ws/src/2025_competition/enph353/enph353_utils/scripts/score_tracker.py")
-        self.score_tracker_process = subprocess.Popen(["python3", script],
-                                                      cwd = os.path.dirname(script)) 
-    
 
+    ##
+    # Launches the ./score_tracker.py in the '~/ros_ws/2025_competion' directory when the button on GUI is clicked. 
+    #
+    # When the script is already running and the user clicks the launch button, 
+    # the running script will be terminated before relaunching another script.
+    # 
+    # @param self The object pointer
+    def SLOT_launch_score_tracker_script(self):
+        
+        script = os.path.expanduser(
+                "~/ros_ws/src/2025_competition/enph353/enph353_utils/scripts/score_tracker.py")
+        
+        if not self.score_tracker_process:
+            
+            self.score_tracker_process = subprocess.Popen(["python3", script],
+                                                        cwd = os.path.dirname(script)) 
+        else:
+            self.score_tracker_process.terminate()
+            self.score_tracker_process.kill()
+            self.score_tracker_process = subprocess.Popen(["python3", script],
+                                                        cwd = os.path.dirname(script))
+            rospy.loginfo("Reopening the Score Tracker Window")
+    
+    ##
+    # Launches the ./state_machine.py when the button on GUI is clicked. 
+    # When the script is already running and the user clicks the launch button, 
+    # the running script will be terminated before relaunching another script.
+    # 
+    # @param self The object pointer
     def SLOT_launch_state_machine_script(self):
-        self.state_machine_process = subprocess.Popen(["./state_machine.py"])
+
+        if not self.state_machine_process:
+            self.state_machine_process = subprocess.Popen(["./state_machine.py"])
+        else:
+            self.state_machine_process.terminate()
+            self.state_machine_process.kill()
+            self.state_machine_process = subprocess.Popen(["./state_machine.py"])
+            rospy.loginfo("Restarting State Machine")
 
 
     ## 
     # Closes all the background processes (score_tracker.py and state_machine.py) when controller_GUI.py is terminated 
     # Uses Qt's event-handling system
-    #
+    # 
+    # @param self The object pointer
+    # @param event Qt's even handler
     def closeEvent(self, event):
         if self.score_tracker_process:
             try:
